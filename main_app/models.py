902f62dee7c55a8c8493e2e1e154335c6eb7415c
Stanislav Lysov <snide.pancake@gmail.com>
2021-03-25 18:24:35 +0300

    add some code

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 
 
+class OneTimeParticipationReport(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название мероприятия')
+    work = models.TextField(verbose_name='Выполненные работы по мероприятию')
+    # date = models.DateField(verbose_name='Дата мероприятия')
+
+
+class OneTimeParticipationApp(models.Model):
+    report = models.OneToOneField(OneTimeParticipationReport, verbose_name='Отчёт', on_delete=models.CASCADE)
+    responsible = models.CharField(max_length=255, verbose_name='ФИО и должность лица, подтверждающего участие')  # В лучшем случае foreignkey, ссылающийся на дядю или тетю
+    is_organizer = models.BooleanField(verbose_name='Организатор', default=False)
+    is_co_organizer = models.BooleanField(verbose_name='Соорганизатор', default=False)
+    is_assistant = models.BooleanField(verbose_name='Помощь в организации', default=False)
+
+    def save(self, *args, **kwargs):
+        error = int(self.is_organizer) + int(self.is_co_organizer) + int(self.is_assistant)
+        if error != 1:
+            raise ValueError('test exception')
+        super().save(*args, **kwargs)
+
+
 class Application(models.Model):
     # owner= models.ForeignKey
     title = models.CharField(max_length=255, verbose_name='Название мероприятия')
