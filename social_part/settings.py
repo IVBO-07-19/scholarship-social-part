8bab42d24697575b7221e43ad96e5076569bb985
Konstantin Suroegin <sk.schooldude@gmail.com>
2021-04-21 19:09:32 +0300

    added postgresql

--- social_part/settings.py
+++ social_part/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+
 # from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,11 +83,22 @@ WSGI_APPLICATION = 'social_part.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':
+        {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'django_db',
+            'USER': 'bojan',
+            'PASSWORD': '789256',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
 }
 
 # Password validation
